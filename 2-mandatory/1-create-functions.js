08e67dacd5dcc8e48dd5017dcc2f8bad5989f29c
Aaron-CYF <86688603+Aaron-CYF@users.noreply.github.com>
2021-09-23 15:38:30 +0100

    finished all exercises

--- 2-mandatory/1-create-functions.js
+++ 2-mandatory/1-create-functions.js
@@ -3,7 +3,8 @@ Write a function that:
 - Accepts an array as a parameter.
 - Returns a new array containing the first five elements of the passed array.
 */
-function first5() {
+function first5(arr) {
+  return arr.slice(0, 4)
 }
 
 /*
@@ -11,7 +12,8 @@ Write a function that:
 - Accepts an array as a parameter.
 - Returns a new array containing the same elements, except sorted.
 */
-function sortArray() {
+function sortArray(arr) {
+  return arr.sort();
 }
 
 /*
@@ -24,7 +26,8 @@ Write a function that:
 - Removes any forward slashes (/) in the strings.
 - Makes the strings all lowercase.
 */
-function tidyUpString() {
+function tidyUpString(arrStrings) {
+   return arrStrings.map(str => str.trim().replace(/\//g, "").toLowerCase);
 }
 
 /*
@@ -33,7 +36,8 @@ Write a function that:
 - Returns a new array containing the same elements, but without the element at the passed index.
 */
 
-function remove() {
+function remove(arr, index) {
+  return arr.splice(index);
 }
 
 /*
@@ -44,7 +48,8 @@ Write a function that:
 - Numbers greater 100 must be replaced with 100.
 */
 
-function formatPercentage() {
+function formatPercentage(arrNums) {
+  return arrNums.map(num => num > 100 ? "100%" : Math.round(num * 100) / 100 + "%");
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
